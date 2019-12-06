e33935f754e9c2f88ccf850257ed599b2964f28a
David Fox <foxdk@ldschurch.org>
2019-12-06 13:57:40 -0700

    Direct and REST endpoint calls with mocked server running in the same test

--- src/test/java/org/familysearch/david/fox/MyFirstEndpointTest.java
+++ src/test/java/org/familysearch/david/fox/MyFirstEndpointTest.java
@@ -7,22 +7,30 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.client.RestTemplate;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // todo: is this the best example of a simple test? go through tutorials
+// todo: this project is really an example of component testing, not integration testing, so rename it
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class MyFirstEndpointTest {
 
   @SuppressWarnings("unused")
@@ -34,6 +42,10 @@ public class MyFirstEndpointTest {
   @Autowired
   private RestTemplate restTemplate;
 
+  @SuppressWarnings("unused")
+  @Autowired
+  private MockMvc mvc;
+
   private MockRestServiceServer mockServer;
 
   @Before
@@ -41,8 +53,7 @@ public class MyFirstEndpointTest {
     this.mockServer = MockRestServiceServer.createServer(restTemplate);
   }
 
-  @Test
-  public void testMyService() {
+  public void testGetAThing_directEndpointCall() {
     String serviceResponseBody = "{'field1': 'abcdef', 'field2': 1234 }";
     String url = "http://some-remote-service/some-path";
     this.mockServer.expect(requestTo(url))
@@ -52,6 +63,19 @@ public class MyFirstEndpointTest {
     ResponseEntity<String> responseEntity = myFirstEndpoint.getAThing();
 
     assertEquals(serviceResponseBody, responseEntity.getBody());
+  }
+
+  @Test
+  public void testGetAThing_restEndpointCall() throws Exception {
+    String serviceResponseBody = "{'field1': 'abcdef', 'field2': 1234 }";
+    String url = "http://some-remote-service/some-path";
+    this.mockServer.expect(requestTo(url))
+        .andExpect(method(HttpMethod.GET))
+        .andRespond(withSuccess(serviceResponseBody, MediaType.APPLICATION_JSON));
+
+    mvc.perform(MockMvcRequestBuilders.get("/first/endpoint").accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string(equalTo(serviceResponseBody)));
   }
 
 }
